3ca7f8607bd4e418dcb1d4c717998f2c93f898c5
fangxi <loveinori@qq.com>
2022-04-18 17:25:44 +0800

    请求头决定是否包装

--- scaling-disco-api/src/main/java/com/re0/disco/api/config/ResultResponseBodyAdvice.java
+++ scaling-disco-api/src/main/java/com/re0/disco/api/config/ResultResponseBodyAdvice.java
@@ -1,6 +1,7 @@
 package com.re0.disco.api.config;
 
 import com.re0.disco.common.result.Result;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -16,6 +17,8 @@ import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJack
 @RestControllerAdvice
 public class ResultResponseBodyAdvice extends AbstractMappingJacksonResponseBodyAdvice {
 
+    private static final String SCALING_DISCO_WEB = "scaling-disco-web";
+
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         if (returnType.getMethod() == null) {
@@ -27,7 +30,8 @@ public class ResultResponseBodyAdvice extends AbstractMappingJacksonResponseBody
 
     @Override
     protected void beforeBodyWriteInternal(MappingJacksonValue bodyContainer, MediaType contentType, MethodParameter returnType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (request.getURI().getPath().startsWith("/api/")) {
+        String source = request.getHeaders().getFirst("source");
+        if (StringUtils.equals(SCALING_DISCO_WEB, source)) {
             Object value = bodyContainer.getValue();
             bodyContainer.setValue(Result.ok(value));
         }
